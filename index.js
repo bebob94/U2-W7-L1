b3e9550bdf3bd47d1c02259c3bbd4ba5ca0cef81
Alberto Macis <bebo.macis@gmail.com>
2023-01-23 17:31:56 +0100

    add files

---
+++ index.js
@@ -0,0 +1,28 @@
+let newName = document.getElementById("name");
+let newSurname = document.getElementById("surname");
+let newDateOfBirth = document.getElementById("DateOfBirth");
+let newButton = document.getElementById("myButton");
+let myTable = document.getElementById("myTable");
+
+const Utente = function () {
+  this.name = "";
+  this.surname = "";
+  this.DateOfBirth = "";
+};
+
+let currentUser = new Utente();
+
+newButton.onclick = function () {
+  currentUser.name = newName.value;
+  currentUser.surname = newSurname.value;
+  currentUser.DateOfBirth = newDateOfBirth.value;
+  let newTdody = document.createElement("tbody");
+  newTdody.innerHTML = `<tr>
+        <td>${currentUser.name}</td>
+        <td>${currentUser.surname}</td>
+        <td>${currentUser.DateOfBirth}</td>
+    </tr>
+  `;
+
+  myTable.appendChild(newTdody);
+};
